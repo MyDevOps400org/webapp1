172e8356e727b582a58804dcd23836eafa245ba9
Frans.Rampai <rampaifrans@gmail.com>
2025-11-07 12:39:35 +0200

    changes made to Add Version 4

--- webapp1/Program.cs
+++ webapp1/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!, Version 3");
+app.MapGet("/", () => "Hello World!, Version 4 Today");
 
 app.Run();
