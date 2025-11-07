9a05654ffa3a9c3b36c28d7c10bfc059e655de81
Frans.Rampai <rampaifrans@gmail.com>
2025-11-07 15:39:19 +0200

    version 13

--- webapp1/Program.cs
+++ webapp1/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!, Version 12");
+app.MapGet("/", () => "Hello World!, Version 13");
 
 app.Run();
